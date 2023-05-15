17877e77745029da263c4e39e214c8bb3986b89d
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-15 19:05:48 +0200

    69-4 Style Banner Component with gradient overlay

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -7,9 +7,22 @@ import img5 from './../../../assets/images/banner/5.jpg';
 import img6 from './../../../assets/images/banner/6.jpg';
 const Banner = () => {
 	return (
-		<div className='carousel w-3/5 h-64 mx-auto'>
+		<div className='carousel w-4/5 h-[550px] mx-auto rounded-lg'>
 			<div id='slide1' className='carousel-item relative w-full'>
-				<img src={img1} className='w-full' />
+				<img src={img1} className='w-full ' />
+				<div className='absolute h-full flex-col space-y-7 p-10 items-center transform bg-gradient-to-r from-[#151515] to-[rgba(21, 21, 21, 0) ] '>
+					<div className='text-6xl text-[white] font-bold'>
+						Affordable Price For Car Servicing
+					</div>
+					<div className='text-[white]'>
+						There are many variations of passages of available, but the majority have{' '}
+						<br />
+						suffered alteration in some form
+					</div>
+					<button className='btn btn-success mr-5'>Success</button>
+
+					<button className='btn btn-outline btn-secondary'>Button</button>
+				</div>
 				<div className='absolute flex transform -translate-y-1/2 left-5 right-5  justify-end  bottom-0'>
 					<a href='#slide4' className='btn btn-circle mr-10'>
 						❮
