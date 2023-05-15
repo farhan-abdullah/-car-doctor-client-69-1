3be48651fbb27ea3ccc90829883ea1031d5edba3
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-15 11:09:01 +0200

    69-2-set-basic-layout-for-footer-and-header

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,9 @@ import { RouterProvider } from 'react-router-dom';
 import router from './Routes/Routes.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-	<React.StrictMode>
-		<RouterProvider router={router} />
-	</React.StrictMode>
+	<div className='mx-auto max-w-7xl'>
+		<React.StrictMode>
+			<RouterProvider router={router} />
+		</React.StrictMode>
+	</div>
 );
