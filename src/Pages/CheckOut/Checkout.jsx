5e7d63ddcfffd81cbc46ef97f49d7b11cb06391f
farhan-abdullah <farhan.abdullah94@gmail.com>
2023-05-16 22:07:25 +0200

    70-4 Extract booking service Data

---
+++ src/Pages/CheckOut/Checkout.jsx
@@ -0,0 +1,84 @@
+import React, { useContext } from 'react';
+import { useLoaderData } from 'react-router';
+import { AuthContext } from '../../AuthProvider/AuthProvider';
+
+const Checkout = () => {
+	const service = useLoaderData();
+	const { user } = useContext(AuthContext);
+	const handleBookService = (event) => {
+		event.preventDefault();
+		const form = event.target;
+		const name = form.name.value;
+		const surname = form.surname.value;
+		const number = form.number.value;
+		const email = form.email.value;
+		const order = {
+			customerName: name,
+			surname,
+			email,
+			number,
+			service: service._id,
+		};
+		console.log(order);
+	};
+	return (
+		<div>
+			<h2 className='text-center'>Book Service: {service.title}</h2>
+			<form onSubmit={handleBookService} className='card-body'>
+				<div className='grid grid-cols-1 md:grid-cols-2 gap-x-6 gap-y-6'>
+					<div className='form-control'>
+						<label className='label'>
+							<span className='label-text'>Your Name</span>
+						</label>
+						<input
+							name='name'
+							type='text'
+							placeholder='Your Name'
+							className='input input-bordered'
+							defaultValue={user?.displayName}
+						/>
+					</div>
+					<div className='form-control'>
+						<label className='label'>
+							<span className='label-text'>Your Surname</span>
+						</label>
+						<input
+							name='surname'
+							type='text'
+							placeholder='Your Surname'
+							className='input input-bordered'
+						/>
+					</div>
+					<div className='form-control'>
+						<label className='label'>
+							<span className='label-text'>Phone Number</span>
+						</label>
+						<input
+							name='number'
+							type='number'
+							placeholder='Your Number'
+							className='input input-bordered'
+						/>
+					</div>
+					<div className='form-control'>
+						<label className='label'>
+							<span className='label-text'>Email</span>
+						</label>
+						<input
+							name='email'
+							type='text'
+							placeholder='email'
+							className='input input-bordered'
+							defaultValue={user?.email}
+						/>
+					</div>
+				</div>
+				<div className='form-control mt-6'>
+					<input className='btn btn-primary' type='submit' value='Place Order' />
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Checkout;
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -3,6 +3,7 @@ import Main from '../Layout/Main';
 import Home from '../Pages/Home/Home/Home';
 import Login from '../Pages/Login/Login';
 import Signup from '../Pages/Signup/Signup';
+import Checkout from '../Pages/CheckOut/Checkout';
 
 const router = createBrowserRouter([
 	{
@@ -20,6 +21,11 @@ const router = createBrowserRouter([
 			{
 				path: '/signup',
 				element: <Signup></Signup>,
+			},
+			{
+				path: 'checkout/:id',
+				element: <Checkout></Checkout>,
+				loader: ({ params }) => fetch(`http://localhost:5000/checkout/${params.id}`),
 			},
 		],
 	},
